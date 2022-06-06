82dfa207c3706bf442237af6d62d84f5bc360b38
Karel Barborak <karel.barborak@bsc-ideas.com>
2022-06-06 23:42:23 +0200

    Assignment init with Spring Boot app

---
+++ src/main/java/com/memsource/MemsourceApplication.java
@@ -0,0 +1,14 @@
+package com.memsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MemsourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemsourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/memsource/MemsourceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.memsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MemsourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
